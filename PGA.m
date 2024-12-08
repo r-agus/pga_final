a8192d950a41fb2dc77a1f4eaff1f753b73daf26
David Andrino <davidandrino2002@gmail.com>
2024-12-08 15:01:15 +0100

    Fase 2

--- PGA.m
+++ PGA.m
@@ -2,18 +2,16 @@
 %% Rubén A. y David A. - Otoño 2024
 
 clear;
-decimales = 4;
+decimales = 5;
 muestras_para_media = 50;
 aproximacion = -3;
 %% Fase 1 - Caracterización de la planta
-% 
-% 
 % *Ejercicio 1*
 % 
 % Capture y caracterice la salida de la planta en función de la tensión de entrada 
-% con un  procedimiento análogo al empleado en la Práctica 2 y obtenga su función 
-% de transferencia  (FdeT) en las unidades que considere más adecuadas para trabajar 
-% en el desarrollo de todo  el diseño. 
+% con un procedimiento análogo al empleado en la Práctica 2 y obtenga su función 
+% de transferencia (FdeT) en las unidades que considere más adecuadas para trabajar 
+% en el desarrollo de todo el diseño. 
 
 ficheros = dir("Fase1\sin_perturbacion\");
 
@@ -88,16 +86,11 @@ tau = mean(cell2mat(taus));
 
 G = Km/(tau*s + 1);
 vpa(G, decimales)
-
 %% 
-% 
-% 
 % *Ejercicio 2*
 % 
 % Corrobore la validez del modelo mediante su simulación en Matlab/Simulink, 
-% como  también se hizo en la Práctica 2
-% 
-% 
+% como también se hizo en la Práctica 2
 
 open_system("Fase1/ejercicio2")
 set_param("ejercicio2/Escalera mecánica", 'Numerator', string(Km), 'Denominator', '[' + string(tau) + ' 1' + ']')
@@ -131,19 +124,17 @@ for i = 1:length(ficheros)
     ylabel("Velocidad escalera (m/s)")
 end
 %% 
-% 
-% 
 % *Ejercicio 3*
 % 
 % Proponga e implemente los siguientes experimentos para caracterizar las alinealidades 
-% de  la planta: 
+% de la planta: 
 %% 
 % # Para modelar la saturación excite el SM con grandes tensiones 
-% # Para modelar  la zona muerta excite el SM con pequeñas tensiones. 
+% # Para modelar la zona muerta excite el SM con pequeñas tensiones. 
 %% 
-% Debe de obtener los valores adecuados  con sus unidades de la saturación y 
-% zona muerta, explicando el procedimiento de obtención,  y añadir al modelo de 
-% Simulink los bloques de las alinealidades caracterizadas  convenientemente colocadas 
+% Debe de obtener los valores adecuados con sus unidades de la saturación y 
+% zona muerta, explicando el procedimiento de obtención, y añadir al modelo de 
+% Simulink los bloques de las alinealidades caracterizadas convenientemente colocadas 
 % y con coherencia de unidades. 
 
 % Valores obtenidos experimentalmente
@@ -163,68 +154,80 @@ syms s
 Gcomp = km_comp/(tau*s + 1);
 vpa(Gcomp, decimales)
 %% 
-% 
-% 
-% 
-% 
 % *Ejercicio 4*
 % 
 % Calcule el equivalente discreto de la planta y caracterice teóricamente su 
-% respuesta ante  entrada escalón, tanto en régimen permanente como transitorio. 
+% respuesta ante entrada escalón, tanto en régimen permanente como transitorio. 
 
-BoG = c2d(tf(Km, [tau 1]), Ts) % BoG = (1-z^(-1)) * sum_(Polos G(s)/s) Res[G(s)/(s*(1-exp(s*Ts)*z^(-1))]
-[~, zp, ~] = zpkdata(BoG, 'v')
+% Se añade un retraso para tener en cuenta el que se quitó en el ejercicio 1
+BoG = series(c2d(tf(Km, [tau 1]), Ts), tf(1, [1 0], Ts)) % BoG = ((1-z^(-1)) * sum_(Polos G(s)/s) Res[G(s)/(s*(1-exp(s*Ts)*z^(-1))]) * (1/z)
+[~, zp, ~] = zpkdata(BoG, 'v');
 
 fprintf("Valor final ante escalón unitario")
 c_disc_inf = dcgain(BoG) % lim(z->1) BoG = 0.02345 / (1 - 0.8779)
 
 fprintf("Tiempo de establecimiento")
-ks_disc = ceil(aproximacion / log(abs(zp))) % Aproximación de log(0.05)/log(abs(zp))
+ks_disc = ceil(aproximacion / log(abs(max(zp)))) + (length(zp) - 1) % Teniendo en cuenta el retardo
 ts_disc = Ts * ks_disc
 %% 
-% 
-% 
 % *Ejercicio 5*
-% 
+
+pert10personas = 0.2;
+%% 
 % Con el número máximo de personas, el escalón unidad no es suficiente para 
-% iniciar el movimiento. La perturbación máxima para el valor positivo es de -0.2m/s, 
-% valor con módulo superior a la salida frente al escalón unidad (0.192 m/s)
-% 
-% 
+% iniciar el movimiento positivo. La perturbación máxima para el valor positivo 
+% es de -0.2m/s, valor con módulo superior a la salida frente al escalón unidad 
+% (0.1993 m/s).
+
+c_inf_pert_pos = max([0, Km*1 - pert10personas])
+%% 
+% Sin embargo, en el movimiento negativo, la perturbación ""ayuda"" a bajar, 
+% por lo que el valor final será de:
+
+c_inf_pert_neg = min([0, Km*(-1) - pert10personas])
 %% Fase 2
 % *Ejercicio 6*
 % 
 % Calcule la FdeT del regulador con sus correspondientes unidades, mediante 
-% la aplicación  detallada del método directo, el resultado debe presentarlo de 
-% la forma factorizada,  polinomial y con potencias negativas en z. Presente la 
-% ecuación en diferencias del  regulador.
+% la aplicación detallada del método directo, el resultado debe presentarlo de 
+% la forma factorizada, polinomial y con potencias negativas en z. Presente la 
+% ecuación en diferencias del regulador.
 
-clear;
+% Normalizar a planta de la forma Kg/(s-pg) = (Km/tau)/(s - (-1/tau)) = Km/(tau*s + 1)
+Kg = Km/tau;
+pg = -1/tau;
 
-decimales = 5;
+clearvars -except Kg pg Ts aproximacion decimales;
 
-% Planta
-Ts=0.035;
-Kg = 0.2345;
-pg = 0.8779;
-aproximacion = -3; %log(0.05)
+% Aproximación del BoG, teniendo en cuenta el retardo adicional
+%% 
+fprintf("Equivalente discreto de la planta a controlar")
+G = tf(Kg, [1 -pg]);
+retardo = tf(1, [1 0], Ts);
+BoG = series(c2d(G, Ts), retardo)
+[NumBoG, DenBoG] = tfdata(BoG, 'v')
 
 % Especificaciones deseadas
 ts = 0.980;
 erpp = 0.03;
 
-fprintf("Parámetros del sistema. Módulo del polo y Km para cada signo")
-pm_mod = exp(Ts*aproximacion/ts) 
-Km_pos = (1 - pm_mod) * (1 - erpp)
-Km_neg = (1 + pm_mod) * (1 - erpp)
+fprintf("===== MÉTODO DE TRUXAL =====\n1. Aplicabilidad del modelo. Aplicable por entrada al escalón y polo menor que la unidad\n2. Causalidad")
+grado_Den_BoG = length(DenBoG) - find(DenBoG ~= 0, 1, 'first');
+grado_Num_BoG = length(NumBoG) - find(NumBoG ~= 0, 1, 'first');
+dif_grados_G = grado_Den_BoG - grado_Num_BoG
+retardos = dif_grados_G - 1;
 
-fprintf("Funciones de transferencia del controlador")
-syms z
-BoG = Kg/(z - pg);
-F_pos = (Km_pos * (z - pg)) / (Kg * (z - pm_mod - Km_pos));
-F1 = vpa(F_pos, decimales)
-F_neg = (Km_neg * (z - pg)) / (Kg * (z + pm_mod - Km_neg));
-F2 = vpa(F_neg, decimales + 1) 
+fprintf("3. Modelo genérico. Se genera un modelo sobreamortiguado teniendo en cuenta la diferencia de grados del paso anterior\n" ...
+    + "\tKm/(z^(-" + retardos + ")*(z-pm))\nCálculo de parámetros:");
+
+ts_prima = ts - retardos*Ts;
+pm = exp(Ts * aproximacion / ts_prima) % Ignorando solución negativa por "no ser práctica"
+km = (1-pm)*(1-erpp)
+M_obj = tf(km, [1, -pm], Ts)*(tf(1, [1 0], Ts)^retardos)
+
+fprintf("4. Cálculo del controlador")
+F = zpk(minreal(M_obj/(BoG * (1 - M_obj))))
+
 %% 
 % Solución:
 % 
@@ -232,8 +235,6 @@ F2 = vpa(F_neg, decimales + 1)
 % z^{-1}}{1-0.9970\cdot z^{-1}} = 0.4203\cdot\frac{z-0.8779}{z-0.9970}$$
 % 
 % $$c[k] = 0.9970\cdot r[k-1] + 0.4203\cdot r[k] - 0.3690\cdot r[k-1]$$
-% 
-% 
 % 
 % $$F_2(z) = \frac{1.8414z-1.6166}{0.2345z-0.22114}=\frac{7.8525-6.8938\cdot 
 % z^{-1}}{1-0.9430\cdot z^{-1}}=7.8525\cdot\frac{z-0.8779}{z-0.9430}$$
@@ -243,85 +244,68 @@ F2 = vpa(F_neg, decimales + 1)
 % *Ejercicio 7*
 % 
 % Dibuje una topología (diagrama de bloques) del sistema de acuerdo a las unidades 
-% elegidas,  que deberán aparecer indicadas en cada señal o secuencia del dibujo. 
+% elegidas, que deberán aparecer indicadas en cada señal o secuencia del dibujo. 
 
-fprintf("Funciones de transferencia del sistema con los reguladores")
-function res = normalize_to_unit_Z(f, z)
-    [num, den] = numden(f);
 
-    coeffs_den = coeffs(den, z);
-    z_coeff = coeffs_den(2);
-    normalized_num = num / z_coeff;
-    normalized_den = den / z_coeff;
-
-    res = vpa(normalized_num / normalized_den, 5);
-end
-
-M_pos = normalize_to_unit_Z(simplifyFraction(BoG*F_pos/(1+BoG*F_pos)), z)
-M_neg = normalize_to_unit_Z(simplifyFraction(BoG*F_neg/(1+BoG*F_neg)), z)
 %% 
 % *Ejercicio 8*
 % 
 % Calcule la FdeT del sistema y caracterice teóricamente su salida ante entradas 
-% escalón de  ±1 m/s. 
+% escalón de ±1 m/s. 
 
-pm_pos = 0.8984;
-km_pos = 0.098555;
+fprintf("Valor de FdeT del sistema controlado")
+M_final = zpk(minreal(feedback(series(F, BoG), 1)))
+[~, p_m_final, ~] = zpkdata(M_final, 'v');
 
-pm_neg = -0.8984;
-km_neg = 1.8414;
+fprintf("Caracterización de salida frente a escalón unidad positivo")
+ts_M = Ts * (ceil(round(aproximacion / log(max(p_m_final)), 10)) + (length(p_m_final) - 1)) % ceil(round(...)) para evitar que x + 1e-12 suba a x + 1
+Km_final = dcgain(M_final) % lim_(z->1) M_final
+c_inf_mas_1V   = Km_final * 1
+c_inf_menos_1V = Km_final * (-1)
 
-% Queda una muestra de más en ts xq se usa la aproximación y por solo tener
-% cuatro decimales en el polo. Si se usan todos los decimales o se utiliza
-% log(0.05) se obtiene el resultado exacto
-fprintf("Caracterización Mpos")
-ts_pos = Ts * ceil(aproximacion/log(abs(pm_pos)))
-c_inf_pos = km_pos / (1 - pm_pos)
-erpp_pos = 1 - c_inf_pos
-
-fprintf("Caracterización Mneg")
-ts_neg = Ts * ceil(aproximacion/log(abs(pm_neg)))
-c_inf_neg = km_neg / (1 - pm_neg)
-erpp_neg = 1 - c_inf_neg
 %% 
 % *Ejercicio 9*
 % 
 % Calcule en régimen permanente tanto la acción de control como la velocidad 
-% de la escalera  subiendo y bajando en diferentes condiciones de carga de la 
-% escalera, para consignas de  ±1 m/s. Relacione los incrementos o decrementos 
-% de acción de control con el peso de las  personas presentes en la escalera. 
+% de la escalera subiendo y bajando en diferentes condiciones de carga de la escalera, 
+% para consignas de ±1 m/s. Relacione los incrementos o decrementos de acción 
+% de control con el peso de las personas presentes en la escalera. 
 
-pert_5personas = 0.1;
-pert_10personas = 0.2;
-entrada = 1;
+pert_5personas = -0.1;
+pert_10personas = -0.2;
 %% 
 % Aplicando teorema de superposición
 
-M_r_pos = M_pos;
-M_p_pos = normalize_to_unit_Z(-1/(1+BoG*F_pos), z)
+M_r = M_final;
+M_p = zpk(minreal(feedback(1, series(F, BoG))));
 
-M_r_neg = M_neg;
-M_p_neg = normalize_to_unit_Z(-1/(1+BoG*F_neg), z)
-
-K0_r_pos = round(subs(M_r_pos, z, 1), decimales)
-K0_p_pos = round(subs(M_p_pos, z, 1), decimales)
-
-K0_r_neg = round(subs(M_r_neg, z, 1), decimales)
-K0_n_neg = round(subs(M_p_neg, z, 1), decimales)
 %% 
 % La perturbación no afecta al tiempo de establecimiento, solo al valor final 
 % (y por tanto al error)
 
-K0_ref = subs(M_r_pos, z, 1);
-K0_pert= subs(M_p_pos, z, 1);
+K0_ref = dcgain(M_r);
+K0_pert= dcgain(M_p);
+K0_F   = dcgain(F);
 
-salida_5personas = round(K0_ref * entrada + K0_pert * pert_5personas, decimales)
-erpp_5personas = round((entrada - K0_ref * entrada - K0_pert * pert_5personas)/entrada, decimales)
+entradas       = [1 -1];
+perturbaciones = [0 -0.1 -0.2];
+perturbaciones_names = [" 0 personas", " 5 personas", "10 personas"];
 
-salida_10personas = round(K0_ref * entrada + K0_pert * pert_10personas, decimales)
-erpp_10personas = round((entrada - K0_ref * entrada - K0_pert * pert_10personas)/entrada, decimales)
+fprintf("Cálculo de error y acción en régimen permanente para distintas perturbaciones\n")
+
+for i = 1:length(entradas)
+    fprintf("\nPara escalón de %0.2fV:\n", entradas(i))
+    for j = 1:length(perturbaciones)
+        r = entradas(i);
+        p = perturbaciones(j);
+
+        c = K0_ref * r + K0_pert * p;
+        e = c - r;
+        a = K0_F * e;
+        
+        fprintf("\tCon %s:\tc_inf=%.03f\terpp=%.03f\ta_inf=%.03f\n", perturbaciones_names(j), c, e, a);
+    end
+end
 
 %% Fase 3
 % *Ejercicio 10*
-% 
-%
